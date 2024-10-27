dabf6e1eb9b9f7fe2b0e1828e2cec29e75325cbd
unit-404 <conway24b@gmail.com>
2024-10-27 21:28:29 +0700

    don't depend

--- src/$core$/Time.ts
+++ src/$core$/Time.ts
@@ -1,4 +1,6 @@
-import {UUIDv4} from "../utils/Utils.ts";
+export const UUIDv4 = () => {
+    return (crypto?.randomUUID ? crypto?.randomUUID() : ("10000000-1000-4000-8000-100000000000".replace(/[018]/g, c => (+c ^ (crypto.getRandomValues(new Uint8Array(1))[0] & (15 >> (+c / 4)))).toString(16))));
+};
 
 //
 export default class AxTime {
