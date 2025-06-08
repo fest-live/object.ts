f1560804f50f87064654ec6c54f4587318f9c835
u2re-dev <c24b@u2re.ru>
2025-06-08 19:20:24 +0700

    Vite Fix

--- src/index.ts
+++ src/index.ts
@@ -2,4 +2,4 @@ export * from "./$core$/AssignObject";
 export * from "./$core$/Mainline";
 export * from "./$core$/Primitives";
 export * from "./$core$/Array";
-export {safe} from "./$core$/Utils";
+export {safe, unwrap} from "./$core$/Utils";
