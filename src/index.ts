88c02e2c49127abb81c371fcdf541e377bde3bf0
u2re-dev <c24b@u2re.ru>
2025-10-24 19:07:57 +0700

    Export

--- src/index.ts
+++ src/index.ts
@@ -6,6 +6,6 @@ export * from "../../../projects/core.ts/src/utils/Promised";
 export * from "../../../projects/core.ts/src/utils/WRef";
 
 //
-export {$triggerLess, $trigger} from "./$wrap$/Symbol";
-export {safe, unwrap, deref, addToCallChain, wrapSetAsArray} from "./$wrap$/Utils";
+export { $triggerLess, $trigger, $subscribe } from "./$wrap$/Symbol";
+export { safe, unwrap, deref, addToCallChain, wrapSetAsArray } from "./$wrap$/Utils";
 export { type refValid, type subValid } from "./$wrap$/Utils";
